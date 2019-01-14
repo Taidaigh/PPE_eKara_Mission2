7aa2fcdea102dc1ad78da9db179965d2431e609d
Taidaigh <pierre.dealmeida@yahoo.fr>
2019-01-15 00:58:42 +0100

    form1

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -35,7 +35,13 @@
             this.GridViewCrit = new System.Windows.Forms.DataGridView();
             this.ClmCritere = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ClmCoef = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lstCrit = new System.Windows.Forms.ListBox();
+            this.txtBoxCrit = new System.Windows.Forms.TextBox();
+            this.txtBoxCritCoef = new System.Windows.Forms.TextBox();
+            this.groupBoxCrit = new System.Windows.Forms.GroupBox();
+            this.AddCrit = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.GridViewCrit)).BeginInit();
+            this.groupBoxCrit.SuspendLayout();
             this.SuspendLayout();
             // 
             // lstOffre
@@ -71,11 +77,12 @@
             // 
             // btnAddCrit
             // 
-            this.btnAddCrit.Location = new System.Drawing.Point(299, 278);
+            this.btnAddCrit.Enabled = false;
+            this.btnAddCrit.Location = new System.Drawing.Point(42, 85);
             this.btnAddCrit.Name = "btnAddCrit";
-            this.btnAddCrit.Size = new System.Drawing.Size(227, 109);
+            this.btnAddCrit.Size = new System.Drawing.Size(145, 49);
             this.btnAddCrit.TabIndex = 7;
-            this.btnAddCrit.Text = "Ajouter un critère";
+            this.btnAddCrit.Text = "Modifier";
             this.btnAddCrit.UseVisualStyleBackColor = true;
             this.btnAddCrit.Click += new System.EventHandler(this.btnAddCrit_Click);
             // 
@@ -86,7 +93,7 @@
             this.ClmCritere,
             this.ClmCoef});
             this.GridViewCrit.Enabled = false;
-            this.GridViewCrit.Location = new System.Drawing.Point(299, 83);
+            this.GridViewCrit.Location = new System.Drawing.Point(556, 72);
             this.GridViewCrit.Name = "GridViewCrit";
             this.GridViewCrit.Size = new System.Drawing.Size(243, 150);
             this.GridViewCrit.TabIndex = 8;
@@ -101,18 +108,71 @@
             this.ClmCoef.HeaderText = "Coef";
             this.ClmCoef.Name = "ClmCoef";
             // 
+            // lstCrit
+            // 
+            this.lstCrit.FormattingEnabled = true;
+            this.lstCrit.Location = new System.Drawing.Point(299, 72);
+            this.lstCrit.Name = "lstCrit";
+            this.lstCrit.Size = new System.Drawing.Size(205, 147);
+            this.lstCrit.TabIndex = 9;
+            this.lstCrit.SelectedIndexChanged += new System.EventHandler(this.listCrit_SelectedIndexChanged);
+            // 
+            // txtBoxCrit
+            // 
+            this.txtBoxCrit.Enabled = false;
+            this.txtBoxCrit.Location = new System.Drawing.Point(10, 49);
+            this.txtBoxCrit.Name = "txtBoxCrit";
+            this.txtBoxCrit.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxCrit.TabIndex = 10;
+            // 
+            // txtBoxCritCoef
+            // 
+            this.txtBoxCritCoef.Enabled = false;
+            this.txtBoxCritCoef.Location = new System.Drawing.Point(139, 49);
+            this.txtBoxCritCoef.Name = "txtBoxCritCoef";
+            this.txtBoxCritCoef.Size = new System.Drawing.Size(76, 20);
+            this.txtBoxCritCoef.TabIndex = 11;
+            this.txtBoxCritCoef.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxCritCoef_KeyPress);
+            // 
+            // groupBoxCrit
+            // 
+            this.groupBoxCrit.Controls.Add(this.btnAddCrit);
+            this.groupBoxCrit.Controls.Add(this.txtBoxCrit);
+            this.groupBoxCrit.Controls.Add(this.txtBoxCritCoef);
+            this.groupBoxCrit.Location = new System.Drawing.Point(289, 281);
+            this.groupBoxCrit.Name = "groupBoxCrit";
+            this.groupBoxCrit.Size = new System.Drawing.Size(229, 151);
+            this.groupBoxCrit.TabIndex = 12;
+            this.groupBoxCrit.TabStop = false;
+            this.groupBoxCrit.Text = "Modifier/Ajouter un critère";
+            // 
+            // AddCrit
+            // 
+            this.AddCrit.Enabled = false;
+            this.AddCrit.Location = new System.Drawing.Point(299, 236);
+            this.AddCrit.Name = "AddCrit";
+            this.AddCrit.Size = new System.Drawing.Size(205, 23);
+            this.AddCrit.TabIndex = 13;
+            this.AddCrit.Text = "Mode ajouter un critère";
+            this.AddCrit.UseVisualStyleBackColor = true;
+            this.AddCrit.Click += new System.EventHandler(this.AddCrit_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 450);
+            this.ClientSize = new System.Drawing.Size(966, 460);
+            this.Controls.Add(this.AddCrit);
+            this.Controls.Add(this.groupBoxCrit);
+            this.Controls.Add(this.lstCrit);
             this.Controls.Add(this.GridViewCrit);
-            this.Controls.Add(this.btnAddCrit);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lstOffre);
             this.Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.GridViewCrit)).EndInit();
+            this.groupBoxCrit.ResumeLayout(false);
+            this.groupBoxCrit.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,6 +187,11 @@
         private System.Windows.Forms.DataGridView GridViewCrit;
         private System.Windows.Forms.DataGridViewTextBoxColumn ClmCritere;
         private System.Windows.Forms.DataGridViewTextBoxColumn ClmCoef;
+        private System.Windows.Forms.ListBox lstCrit;
+        private System.Windows.Forms.TextBox txtBoxCrit;
+        private System.Windows.Forms.TextBox txtBoxCritCoef;
+        private System.Windows.Forms.GroupBox groupBoxCrit;
+        private System.Windows.Forms.Button AddCrit;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -34,19 +34,81 @@ namespace WindowsFormsApp1
 
         private void lstOffre_SelectedIndexChanged(object sender, EventArgs e)
         {
-            GridViewCrit.Rows.Clear();
+            lstCrit.Items.Clear();
             foreach (KeyValuePair<Critere, double> o in DAOCritere.GetCritereCoefByOffre(conn,lstOffre.SelectedIndex+1))
             {
-                string[] row = { o.Key.Libelle, o.Value.ToString() };
-                GridViewCrit.Rows.Add(row);
+                lstCrit.Items.Add(o.Key.Libelle);
             }
             btnAddCrit.Enabled = true;
+            txtBoxCritCoef.Enabled = true;
+            txtBoxCrit.Enabled = true;
+            AddCrit.Enabled = true;
+            groupBoxCrit.Text = "Ajouter un critère";
+            btnAddCrit.Text = "Ajouter";
         }
 
         private void btnAddCrit_Click(object sender, EventArgs e)
         {
             Form2 f2 = new Form2(conn, lstOffre.SelectedIndex+1);
             f2.Show();
+            if (txtBoxCrit.Enabled == true)
+            {
+                //Add
+            }
+            else
+            {
+                //Modif
+            }
+        }
+
+        private void listCrit_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnAddCrit.Enabled = false;
+            txtBoxCritCoef.Enabled = true;
+            txtBoxCrit.Enabled = false;
+            groupBoxCrit.Text = "Modifier un critère";
+            btnAddCrit.Text = "Modifier";
+
+            foreach (KeyValuePair<Critere, double> o in DAOCritere.GetCritereCoefByOffre(conn, lstOffre.SelectedIndex + 1))
+            {
+                if (o.Key.Libelle == lstCrit.Text)
+                {
+                    txtBoxCrit.Text = o.Key.Libelle;
+                    txtBoxCritCoef.Text = o.Value.ToString();
+                }                
+            }
+        }
+
+        private void txtBoxCritCoef_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (ch == 46 && txtBoxCritCoef.Text.IndexOf('.') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void AddCrit_Click(object sender, EventArgs e)
+        {
+            btnAddCrit.Enabled = true;
+            txtBoxCritCoef.Enabled = true;
+            txtBoxCrit.Enabled = true;
+
+            groupBoxCrit.Text = "Ajouter un critère";
+            btnAddCrit.Text = "Ajouter";
+            lstCrit.SelectedIndex = -1;
+            txtBoxCrit.Clear();
+            txtBoxCritCoef.Clear();
+            btnAddCrit.Enabled = true;
+            txtBoxCritCoef.Enabled = true;
+            txtBoxCrit.Enabled = true;
         }
 
         private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
