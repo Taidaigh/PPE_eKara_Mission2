70727c36be3c1579913cb857a9bad66bba75ef25
Taidaigh <pierre.dealmeida@yahoo.fr>
2019-01-20 14:18:32 +0100

    r

--- WindowsFormsApp1/WindowsFormsApp1/DAOCritEval.cs
+++ WindowsFormsApp1/WindowsFormsApp1/DAOCritEval.cs
@@ -12,8 +12,9 @@ namespace WindowsFormsApp1
 		public static void SetNote(NpgsqlConnection conn, int offre, string nom_candid, string prenom_candid,int note,string nom_rh, string prenom_rh, string libelle_crit)
         {
             int eval = -1;
+            string sql = "SELECT COUNT(id_evaluation) FROM EVALUATION WHERE EVALUATION.nom_rh_evaluation = '" + nom_rh + "' AND EVALUATION.prenom_rh_evaluation = '" + prenom_rh + "' AND id_candidature = (SELECT id_candidature FROM CANDIDATURE WHERE id_offre_emplois = " + offre + " AND nom_candidature = '" + nom_candid + "' AND prenom_candidature = '" + prenom_candid + "'); ";
             //Verifie si il existe une evaluation de ce rh pour cette candid et offre
-            using (NpgsqlCommand cmd = new NpgsqlCommand("SELECT COUNT(id_evaluation) FROM EVALUATION WHERE EVALUATION.nom_rh_evaluation = '" + nom_rh + "' AND EVALUATION.prenom_rh_evaluation = '" + prenom_rh + "' AND id_candidature = (SELECT id_candidature FROM CANDIDATURE WHERE id_offre_emplois = " + offre + " AND nom_candidature = '" + nom_candid + "' AND prenom_candidature = '" + prenom_candid + "'); ", conn))
+            using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
             using (NpgsqlDataReader reader = cmd.ExecuteReader())
             {
                 while (reader.Read())
@@ -53,12 +54,16 @@ namespace WindowsFormsApp1
 
             if (verifnote == 1)
             {
-
+                //Met à jour la note du critere
+                using (NpgsqlCommand cmd = new NpgsqlCommand(" UPDATE NOTER SET note = " + note + " WHERE id_evaluation = (SELECT EVALUATION.id_evaluation FROM EVALUATION JOIN CANDIDATURE ON CANDIDATURE.id_candidature = EVALUATION.id_evaluation WHERE CANDIDATURE.nom_candidature = '" + nom_candid + "' AND CANDIDATURE.prenom_candidature = '" + prenom_candid + "' AND CANDIDATURE.id_offre_emplois = 1 AND EVALUATION.nom_rh_evaluation = '" + nom_rh + "' AND EVALUATION.prenom_rh_evaluation = '" + prenom_rh + "') AND id_critere = (SELECT CRITERE.id_critere FROM CRITERE INNER JOIN ASSOCIER ON ASSOCIER.id_critere = CRITERE.id_critere WHERE ASSOCIER.id_offre_emplois = 1 CRITERE.libelle_critere = '" + libelle_crit + "'  );", conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
             }
             else
             {
                 //Créer la note pour un critere
-                using (NpgsqlCommand cmd = new NpgsqlCommand("INSERT INTO NOTER(note, id_evaluation, id_critere) VALUES(" + note + ", (SELECT EVALUATION.id_evaluation FROM EVALUATION JOIN CANDIDATURE ON CANDIDATURE.id_candidature = EVALUATION.id_evaluation WHERE CANDIDATURE.nom_candidature = '" + nom_candid + "' AND CANDIDATURE.prenom_candidature = '" + prenom_candid + "' AND CANDIDATURE.id_offre_emplois = 1 AND EVALUATION.nom_rh_evaluation = '" + nom_rh + "' AND EVALUATION.prenom_rh_evaluation = '" + prenom_rh + "'),(SELECT CRITERE.id_critere FROM CRITERE INNER JOIN ASSOCIER ON ASSOCIER.id_critere = CRITERE.id_critere WHERE ASSOCIER.id_offre_emplois = 1 AND CRITERE.libelle_critere = '" + libelle_crit + "'  ));", conn))
+                using (NpgsqlCommand cmd = new NpgsqlCommand("INSERT INTO NOTER(note, id_evaluation, id_critere) VALUES(" + note + ", (SELECT EVALUATION.id_evaluation FROM EVALUATION JOIN CANDIDATURE ON CANDIDATURE.id_candidature = EVALUATION.id_evaluation WHERE CANDIDATURE.nom_candidature = '" + nom_candid + "' AND CANDIDATURE.prenom_candidature = '" + prenom_candid + "' AND CANDIDATURE.id_offre_emplois = " + offre + " AND EVALUATION.nom_rh_evaluation = '" + nom_rh + "' AND EVALUATION.prenom_rh_evaluation = '" + prenom_rh + "'),(SELECT CRITERE.id_critere FROM CRITERE INNER JOIN ASSOCIER ON ASSOCIER.id_critere = CRITERE.id_critere WHERE ASSOCIER.id_offre_emplois = 1 AND CRITERE.libelle_critere = '" + libelle_crit + "'  ));", conn))
                 {
                     cmd.ExecuteNonQuery();
                 }
